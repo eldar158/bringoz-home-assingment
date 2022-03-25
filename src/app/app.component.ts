81da3b84bd4bb2a6554e81f4a04dd83cd04b5b3e
eldar <eldar158@gmail.com>
2022-03-25 19:38:13 +0300

    first working map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,7 @@ import { environment } from './../environments/environment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  apiKey = environment.apiKey;
+  title = 'bringoz-home-project';
+  lat = 32.0700102;
+  lng = 34.8159896;
 }
